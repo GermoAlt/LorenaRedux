ab48148a917a2be2f053374ec9f893643c5fa60e
noodle <santiago@altairac.dev>
2025-03-25 01:13:06 -0700

    add managed entity

--- src/main/java/dev/altairac/lorenaredux/model/Lore.java
+++ src/main/java/dev/altairac/lorenaredux/model/Lore.java
@@ -1,5 +1,10 @@
 package dev.altairac.lorenaredux.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Lore {
+    @Id
     private Long messageId;
 }
